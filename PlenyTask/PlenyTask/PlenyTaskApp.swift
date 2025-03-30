26a90b01c15af8867622753da9860c173caa33c9
Ahmed Azab <ahmed.azab.003@outlook.com>
2025-03-30 15:16:10 +0200

    add swift ui project

---
+++ PlenyTask/PlenyTask/PlenyTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlenyTaskApp.swift
+//  PlenyTask
+//
+//  Created by Ahmed Azab on 30/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlenyTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlenyTask/PlenyTaskTests/PlenyTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlenyTaskTests.swift
+//  PlenyTaskTests
+//
+//  Created by Ahmed Azab on 30/03/2025.
+//
+
+import Testing
+@testable import PlenyTask
+
+struct PlenyTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
